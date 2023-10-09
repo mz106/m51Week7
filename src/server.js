036370c6dca0e9d1998b56c65d5ed8aae25c2eff
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-10-09 17:12:46 +0100

    add post route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,44 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/books", (request, response) => {
+  console.log(request.originalUrl);
+  const book = {
+    title: "lord of the rings",
+    author: "tolkein",
+    genre: "fantasy",
+  };
+
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
+
+app.post("/books", (request, response) => {
+  console.log(request.body.title);
+  const newBook = {
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  };
+
+  // create a book on the db(
+  //  title: request.body.title,
+  // )
+
+  const successResponse = {
+    message: "success",
+    newBook: newBook,
+  };
+
+  response.send(successResponse);
+});
+
 app.listen(5001, () => {
   console.log("Server is listening");
 });
