3e64295e677ece7ce06c48265c69d7da6bb8fd7c
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-10-09 15:21:06 +0100

    add static routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// http://localhost:5001/
+// app.use("/", express.static("blank"));
+
+// http://localhost:5001/example
+app.use("/", express.static("example"));
+
+app.use("/beansontoast", express.static("beansontoast"));
+
 app.listen(5001, () => {
   console.log("Server is listening");
 });
