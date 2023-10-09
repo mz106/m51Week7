182a3dce92b13a6a81a7d99bc52f00c44240d7d2
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-10-09 15:37:18 +0100

    chore: remove static routes server.js

--- src/server.js
+++ src/server.js
@@ -2,14 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// http://localhost:5001/
-// app.use("/", express.static("blank"));
-
-// http://localhost:5001/example
-app.use("/", express.static("example"));
-
-app.use("/beansontoast", express.static("beansontoast"));
-
 app.listen(5001, () => {
   console.log("Server is listening");
 });
